07f70bdfaa6fb6315f25a3f743269431de446ac1
Punam Das <145331728+pritam123junior@users.noreply.github.com>
2024-03-22 17:08:38 +0600

    Update admin.entity.ts

--- MID PROJECT/Hospital Management System/project/src/admin/admin.entity.ts
+++ MID PROJECT/Hospital Management System/project/src/admin/admin.entity.ts
@@ -1,17 +1,66 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
 
-@Entity()
-export class AdminEntity{
+@Entity('Doctor_Info')
+export class DoctorEntity{
     @PrimaryGeneratedColumn()
-    id: number;
+    DoctorID:number;
 
     @Column({ length: 100 })
     fullName: string;
-
+    @Column({ length: 256 })
+    Qualification:string;
     @Column({ unsigned: true })
-    age: number;
+    salary: number;
+    @Column({ length: 124 })
+    Email: string;
+    @Column({length:130})
+    PhoneNumber:string
+    @Column({unsigned:true})
+    Schedule:string;
 
-    @Column({ default: 'active', enum: ['active', 'inactive'] })
-    status: string;
+}
+export class AdminEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    name: string;
+  
+    @Column()
+    @Unique(['email'])
+    email: string;
+  
+    @Column()
+    password: string;
+  
+    @Column()
+    dob: Date;
+  
+    @Column()
+    phone: string;
+  
+    @Column()
+    address: string;
+  
+    @Column()
+    imageUrl: string;
+  }
+  
+  @Entity('OTP')
+  export class OTP_Entity {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    otp: string;
+  
+    @Column()
+    email: string;
+  
+    @Column()
+    status: number;
+  }
+@Entity('Nurse_info')
+export class NurseEntity{
 
 }
