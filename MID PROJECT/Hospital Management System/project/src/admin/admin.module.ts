8488008563e46b206a76b160f43988f404dbafde
Punam Das <145331728+pritam123junior@users.noreply.github.com>
2024-03-22 17:10:27 +0600

    Update admin.module.ts

--- MID PROJECT/Hospital Management System/project/src/admin/admin.module.ts
+++ MID PROJECT/Hospital Management System/project/src/admin/admin.module.ts
@@ -1,10 +1,28 @@
 import { Module } from "@nestjs/common";
 import { AdminController } from "./admin.controller";
 import { AdminService } from "./admin.service";
+import { AdminEntity, DoctorEntity, OTP_Entity } from "./admin.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+import { MailerModule } from "@nestjs-modules/mailer";
 
 
 @Module({
-
+    imports: [  ConfigModule.forRoot(),
+        MailerModule.forRoot({
+          transport: {
+            host: 'smtp.gmail.com',
+            port: 465,
+            // ignoreTLS: true,
+            secure: true,
+            auth: {
+              user: process.env.EMAIL,
+              pass: process.env.MAIL_PASS,
+            },
+            tls: { rejectUnauthorized: false },
+          },
+        }),
+        TypeOrmModule.forFeature([ DoctorEntity,AdminEntity,OTP_Entity]),],
 controllers: [AdminController],
 providers:[AdminService],
 })
