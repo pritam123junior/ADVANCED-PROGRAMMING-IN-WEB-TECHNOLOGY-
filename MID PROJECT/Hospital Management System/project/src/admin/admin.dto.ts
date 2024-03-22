dde5272b8dda71848a16b4aaf19bfb258ed5a262
Punam Das <145331728+pritam123junior@users.noreply.github.com>
2024-03-22 17:07:35 +0600

    Update admin.dto.ts

--- MID PROJECT/Hospital Management System/project/src/admin/admin.dto.ts
+++ MID PROJECT/Hospital Management System/project/src/admin/admin.dto.ts
@@ -1,37 +1,59 @@
-import { IsEmail, IsEnum, IsInt, IsNotEmpty, IsPhoneNumber, IsString, Length, Matches } from "class-validator";
+import { IsDateString, IsEmail, IsEnum, IsInt, IsNotEmpty, IsPhoneNumber, IsString, Length, Matches, MinLength } from "class-validator";
 
-/*export class NurseDTO{
-   
-      @IsString()
-      @IsNotEmpty()
-      @Matches(/.{4,}/, { message: 'Name should be at least 4 characters long' })
-      name: string;
-    
-      @IsEmail()
-      @IsNotEmpty()
-      @Matches(/.*@aiub.edu$/, { message: 'Email must be from aiub.edu domain' })
-      email: string;
-    
-      @IsNotEmpty()
-      @Matches(/^(?=.*[A-Z]).{6,}$/, { message: 'Password must be at least 6 characters long and contain one uppercase character' })
-      password: string;
-    
-      @IsEnum(['male', 'female'], { message: 'Gender must be either "male" or "female"' })
-      gender: string;
-    
-      
-    
-      @IsPhoneNumber('BD')
-      phoneNumber: number;
-    }*/
 
     export class CreateUserDto {
+      DoctorID: number;
       fullName: string;
-      age: number;
+      Qualification:string;
+        Salary: number;
+        Email: string;
+        phoneNumber:string;
+        Schedule:string;
     }
     
-    export class UpdateUserStatusDto {
-      userId: number;
-      newStatus: 'active' | 'inactive';
+    export class UpdateUserDto {
+      DoctorID: number;
+      Schedule: string;//'Mornning' | 'Evenning'|'Night';
+      Salary: number;
+    }
+    export class DeleteUserDto{
+      DoctorID:number;
+    }
+    export class AdminDTO {
+      @IsNotEmpty({ message: 'Name is required' })
+      @IsString({ message: 'Name must be a string' })
+      name: string;
+    
+      @IsEmail({}, { message: 'Email must be a valid email' })
+      email: string;
+    
+      @IsString()
+      @MinLength(6, { message: 'Password is too short' })
+      @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'Password is too weak',
+      })
+      password: string;
+    
+      @IsDateString({ strict: true }, { message: 'Date of birth must be a date' })
+      dob: Date;
+    
+      @IsPhoneNumber('BD', { message: 'Phone number must be a valid phone number' })
+      phone: string;
+    
+      @IsString({ message: 'Address must be a string' })
+      address: string;
+    
+      @IsString({ message: 'Image url must be a string' })
+      imageUrl: string;
     }
     
+    export class OTP_DTO {
+      @IsNotEmpty({ message: 'OTP is required' })
+      @IsString({ message: 'OTP must be a string' })
+      otp: string;
+    
+      @IsEmail({}, { message: 'Email must be a valid email' })
+      email: string;
+    
+      status: number;
+    }
