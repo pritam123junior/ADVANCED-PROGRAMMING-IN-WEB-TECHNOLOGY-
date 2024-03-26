6d4b27cee55facdc02e8b6e79a1eb0543cfcf819
Punam Das <145331728+pritam123junior@users.noreply.github.com>
2024-03-26 17:36:40 +0600

    Update admin.controller.ts

--- MID PROJECT/Hospital Management System/project/src/admin/admin.controller.ts
+++ MID PROJECT/Hospital Management System/project/src/admin/admin.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Delete, Get, HttpException, Param, Patch, Post, Query, Session, UploadedFile, UseGuards, UseInterceptors, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpException, Param, Patch, Post, Put, Query, Res, Session, UploadedFile, UseGuards, UseInterceptors, UsePipes, ValidationPipe } from "@nestjs/common";
 import { AdminService } from "./admin.service";
-import { AdminDTO, CreateUserDto, UpdateUserDto } from "./admin.dto";
-import { AdminEntity } from "./admin.entity";
+import { AdminDTO, CreateUserDto, UpdateUserDto, imageDTO } from "./admin.dto";
+import { AdminEntity, image_Enity } from "./admin.entity";
 import { User } from "src/nurse/user.entity";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { MulterError, diskStorage } from "multer";
@@ -15,129 +15,99 @@ import { SessionGuard } from "./session.guard";
 export class AdminController {
     constructor(private readonly adminService: AdminService) { }
     
-    @Post('/register')
-    @UseInterceptors(
-      FileInterceptor('avatar', {
-        fileFilter: (req, file, cb) => {
-          if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
-            cb(null, true);
-          } else {
-            cb(new MulterError('LIMIT_UNEXPECTED_FILE', 'image'), false);
-          }
-        },
-        limits: { fileSize: 2097152 },
-        storage: diskStorage({
-          destination: './uploads/admin',
-          filename: function (req, file, cb) {
-            const ext = extname(file.originalname);
-            const fileName: string =
-              file.originalname
-                .replace(ext, '')
-                .toLowerCase()
-                .split(' ')
-                .join('-') +
-              '-' +
-              Date.now();
-            req.body.imageUrl = fileName + ext;
-            cb(null, fileName + ext);
-          },
-        }),
-      }),
-    )
-    @UsePipes(new ValidationPipe())
-    async adminRegistration(
-      @UploadedFile() avatar: Express.Multer.File,
-      @Body() data: AdminDTO,
-    ): Promise<AdminEntity> {
-      console.log(avatar);
-      data.imageUrl = avatar.filename;
-      return this.adminService.adminRegistration(data);
-    }
-  
-    @Post('/signin')
-    async signIn(
-      @Body() data: { email: string; password: string },
-      @Session() session: { email: string; destroy: () => void },
-    ) {
-      const result = await this.adminService.signIn(data);
-      if (!result.isUserExist && !result.isValidPass) {
-        throw new HttpException(result, 400);
-      } else if (result.isValidPass) {
-        session.email = data.email;
-        return result;
-      } else {
-        throw new HttpException(result, 401);
-      }
-      // return this.adminService.signIn(data);
-    }
-  
-    @Get('/signout')
-    signout(@Session() session: { email: string; destroy: () => void }) {
-      session.destroy();
-  
-      return { message: 'Signout successfully' };
-    }
-  
-    @Get('/getadmin')
-    @UseGuards(SessionGuard)
-    getAdmin(@Session() session: { email: string }) {
-      return this.adminService.getAdmin(session.email);
-    }
-  
-    @Get('/getResetPassOtp/:email')
-    async getResetPassOtp(@Param() data: { email: string }) {
-      const result = await this.adminService.sendOTPResetPassword(data.email);
-      if (result.isUserExist) {
-        if (result.error) {
-          throw new HttpException(result, 500);
-        } else {
-          return result;
-        }
-      } else {
-        throw new HttpException(result, 400);
-      }
-    }
-  
-    @Post('/verifyOtp')
-    async verifyOtp(@Body() data: { email: string; otp: string }) {
-      const result = await this.adminService.verifyOTPResetPassword(data);
-      if (!result.isOTPMatched) {
-        throw new HttpException(result, 400);
-      } else {
-        return result;
-      }
-    }
-  
-    @Post('/resetPassword')
-    async resetPassword(
-      @Body() data: { email: string; password: string; otp: string },
-    ) {
-      const result = await this.adminService.resetPassword(data);
-      if (!result.isOTPMatched) {
-        throw new HttpException(result, 400);
-      } else {
-        if (result.isPasswordUpdated) {
-          return result;
-        } else {
-          throw new HttpException(result, 500);
-        }
-      }
-    }
-  
-    @Patch('/updateProfile')
-    @UseGuards(SessionGuard)
-    async updateProfile(
-      @Body() data: AdminDTO,
-      @Session() session: { email: string },
-    ) {
-      const result = await this.adminService.updateProfile(data, session.email);
-      if (result.isProfileUpdated) {
-        return result;
-      } else {
-        throw new HttpException(result, 500);
-      }
-    }
+   
+      @Post('/register')
+     
+      @UsePipes(new ValidationPipe())
+      async adminRegistration(
 
+        @Body() data: AdminDTO,
+      ): Promise<AdminEntity> {
+       
+        return this.adminService.adminRegistration(data);
+      }
+    
+      @Post('/signin')
+      async signIn(
+        @Body() data: { name: string; password: string },
+        @Session() session: { name: string; destroy: () => void },
+        ) {
+          const result = await this.adminService.signIn(data);
+          if (!result.isUserExist && !result.isValidPass) {
+            throw new HttpException(result, 400);
+          } else if (result.isValidPass) {
+            session.name = data.name;
+            return result;
+          } else {
+            throw new HttpException(result, 401);
+          }
+          // return this.adminService.signIn(data);
+        }
+    
+      @Get('/signout')
+      signout(@Session() session: { email: string; destroy: () => void }) {
+        session.destroy();
+    
+        return { message: 'Signout successfully' };
+      }
+    
+      @Get('/getadmin')
+    
+      getAdmin(@Session() session: { email: string }) {
+        return this.adminService.getAdmin(session.email);
+      }
+  
+    
+    
+    
+      @Patch('/updateProfile')
+     
+      async updateProfile(
+        @Body() data: AdminDTO,
+        @Session() session ,
+      ) {
+        const result = await this.adminService.updateProfile(data, session.email);
+        if (result.isProfileUpdated) {
+          return result;
+        } else {
+          throw new HttpException(result, 500);
+        }
+      }
+      @Post('uploadProfilePic')
+      @UseInterceptors(FileInterceptor('file',
+          {
+              fileFilter: (req, file, cb) => {
+                  if (file.originalname.match(/^.*\.(jpg)$/))
+                      cb(null, true);
+                  else {
+                      cb(new MulterError('LIMIT_UNEXPECTED_FILE', 'image'), false);
+                  }
+              },
+              limits: { fileSize: 30000000},
+              storage: diskStorage({
+                  destination: './uploads',
+                  filename: function (req, file, cb) {
+                      cb(null, Date.now() + file.originalname)
+                  },
+              })
+          }))
+      uploadFile(@UploadedFile() file: Express.Multer.File,@Res() res) {
+          console.log(file);
+          if(file){
+              return res.status(200).send('file with extension has been uploaded');
+          }else{
+              return res.status(400).send('Invalid file')
+          }
+        }  @UsePipes(new ValidationPipe())
+        async UploadPic(
+          @UploadedFile() avatar: Express.Multer.File,
+          @Body() data: imageDTO,
+        ): Promise<image_Enity> {
+          console.log(avatar);
+          data.Image = avatar.filename;
+          return this.adminService.addpic(data);
+        }
+    
     
                  @Post('/add_Doctor')
       async createUser(@Body() createUserDto: CreateUserDto) {
@@ -148,29 +118,41 @@ export class AdminController {
       async updateUser(@Body() Data: UpdateUserDto) {
         return this.adminService.updateUser (Data);
       }
-   
-    
-      @Get('/older_than_40')
-      async getUsersOlderThan40() {
-        return this.adminService.getUsersOlderThan40();
-      }
-    
-      @Get('/list')
-      async getAllUsers() {
-        return this.adminService.getAllUsers();
-      }
-      
+
+
       
       @Delete(':id')
       async deleteUser(@Param('id') id: number) {
           await this.adminService.deleteUser(id);
           return { message: 'User deleted successfully.' };
       }
+  
+      @Post('/add_Nurse')
+      async addUser(@Body() createUserDto: CreateUserDto) {
+        return this.adminService.createUser(createUserDto);
+      }
+ 
+      @Patch('/nurse/:id')
+      async updateNurseinfo(@Body() Data: UpdateUserDto) {
+        return this.adminService.updateNurseinfo (Data);
+      }
+
+   
+      
+      @Delete('/nurse/:id')
+      async deletenurseinfo(@Param('id') id: number) {
+          await this.adminService.deleteUser(id);
+          return { message: 'User deleted successfully.' };
+      }
+      @Get('/list')
+      async getAllUsers() {
+        return this.adminService.getAllUsers();
+      }
+      
       @Get('/search/:name')
       searchByname(@Param('name') name: string) {
           return this.adminService.searchByname(name);
       }
-
   }
 
 
